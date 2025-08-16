a92c7848099675ea8432388172307b0afbbd4c9a
Sergio G. Pajares <sergiogarciapajares@gmail.com>
2025-08-16 10:16:20 +0200

    Added translation using Weblate (Spanish)

---
+++ libsiut/libsiut-es.ts
@@ -0,0 +1,147 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es">
+<context>
+    <name>SICard</name>
+    <message>
+        <location filename="src/sicard.cpp" line="42"></location>
+        <source>stationNumber: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/sicard.cpp" line="43"></location>
+        <source>cardNumber: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/sicard.cpp" line="47"></location>
+        <source>check: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/sicard.cpp" line="48"></location>
+        <source>start: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/sicard.cpp" line="49"></location>
+        <source>finish: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SiStationConfig</name>
+    <message>
+        <location filename="src/device/sitask.cpp" line="98"></location>
+        <source>Station number: {{StationNumber}}
+Extended mode: {{ExtendedMode}}
+Auto send: {{AutoSend}}
+Handshake: {{HandShake}}
+Password access: {{PasswordAccess}}
+Read out after punch: {{ReadOutAfterPunch}}
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/device/sitask.cpp" line="107"></location>
+        <location filename="src/device/sitask.cpp" line="108"></location>
+        <location filename="src/device/sitask.cpp" line="109"></location>
+        <location filename="src/device/sitask.cpp" line="110"></location>
+        <location filename="src/device/sitask.cpp" line="111"></location>
+        <source>True</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/device/sitask.cpp" line="107"></location>
+        <location filename="src/device/sitask.cpp" line="108"></location>
+        <location filename="src/device/sitask.cpp" line="109"></location>
+        <location filename="src/device/sitask.cpp" line="110"></location>
+        <location filename="src/device/sitask.cpp" line="111"></location>
+        <source>False</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>siut::CommPort</name>
+    <message>
+        <location filename="src/device/commport.cpp" line="42"></location>
+        <source>Available ports: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/device/commport.cpp" line="50"></location>
+        <source>Connecting to %1 - baudrate: %2, data bits: %3, parity: %4, stop bits: %5</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/device/commport.cpp" line="60"></location>
+        <source>%1 connected OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/device/commport.cpp" line="64"></location>
+        <source>%1 connect ERROR: %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/device/commport.cpp" line="74"></location>
+        <source>%1 closed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/device/commport.cpp" line="91"></location>
+        <source>possible solution:
+Wait at least 10 seconds and then try again.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/device/commport.cpp" line="100"></location>
+        <source>There are no ports available.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/device/commport.cpp" line="103"></location>
+        <source>Selected port %1 is not available.
+List of accessible ports:
+
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>siut::DeviceDriver</name>
+    <message>
+        <location filename="src/device/sidevicedriver.cpp" line="124"></location>
+        <source>Garbage received, stripping %1 characters from beginning of buffer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/device/sidevicedriver.cpp" line="141"></location>
+        <source>NAK received</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/device/sidevicedriver.cpp" line="147"></location>
+        <source>Legacy protocol is not supported, switch station to extended one.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/device/sidevicedriver.cpp" line="154"></location>
+        <source>Valid message shall end with ETX or NAK, throwing data away</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/device/sidevicedriver.cpp" line="170"></location>
+        <source>SIDeviceDriver::sendCommand() - ERROR Sending of EXT commands only is supported for sending.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>siut::SiTask</name>
+    <message>
+        <location filename="src/device/sitask.cpp" line="24"></location>
+        <source>SiCommand timeout after %1 sec.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
