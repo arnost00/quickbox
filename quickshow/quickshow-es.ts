8c16d0c2cc8e6f7f2c66e8c33543dcd2c4558021
Sergio G. Pajares <sergiogarciapajares@gmail.com>
2025-08-16 10:16:20 +0200

    Added translation using Weblate (Spanish)

---
+++ quickshow/quickshow-es.ts
@@ -0,0 +1,48 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es">
+<context>
+    <name>ClassCellRenderer</name>
+    <message>
+        <location filename="src/cellrenderer.cpp" line="77"></location>
+        <source>length: {{length}} climb: {{climb}}</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="src/mainwindow.ui" line="14"></location>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/mainwindow.cpp" line="26"></location>
+        <source>Toggle full screen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/mainwindow.cpp" line="39"></location>
+        <source>Increase font size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/mainwindow.cpp" line="52"></location>
+        <source>Decrease font size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/mainwindow.cpp" line="79"></location>
+        <source>E%1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ResultsCellRenderer</name>
+    <message>
+        <location filename="src/cellrenderer.cpp" line="219"></location>
+        <source>DISQ</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
