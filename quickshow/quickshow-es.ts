f5126d273ec7f9a259bd804ead5c20992c27fbc5
Sergio G. Pajares <sergiogarciapajares@gmail.com>
2025-08-27 14:44:30 +0200

    Translated using Weblate (Spanish)
    Currently translated at 14.2% (1 of 7 strings)
    
    Translation: QuickBox/quickshow
    Translate-URL: https://hosted.weblate.org/projects/quickbox/quickshow/es/

--- quickshow/quickshow-es.ts
+++ quickshow/quickshow-es.ts
@@ -4,35 +4,35 @@
 <context>
     <name>ClassCellRenderer</name>
     <message>
-        <location filename="src/cellrenderer.cpp" line="77"></location>
+        <location filename="src/cellrenderer.cpp" line="77"/>
         <source>length: {{length}} climb: {{climb}}</source>
-        <translation type="unfinished"></translation>
+        <translation>Distancia: {{lenght}} desnivel: {{climb}}</translation>
     </message>
 </context>
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="src/mainwindow.ui" line="14"></location>
+        <location filename="src/mainwindow.ui" line="14"/>
         <source>MainWindow</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="src/mainwindow.cpp" line="26"></location>
+        <location filename="src/mainwindow.cpp" line="26"/>
         <source>Toggle full screen</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="src/mainwindow.cpp" line="39"></location>
+        <location filename="src/mainwindow.cpp" line="39"/>
         <source>Increase font size</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="src/mainwindow.cpp" line="52"></location>
+        <location filename="src/mainwindow.cpp" line="52"/>
         <source>Decrease font size</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="src/mainwindow.cpp" line="79"></location>
+        <location filename="src/mainwindow.cpp" line="79"/>
         <source>E%1</source>
         <translation type="unfinished"></translation>
     </message>
@@ -40,7 +40,7 @@
 <context>
     <name>ResultsCellRenderer</name>
     <message>
-        <location filename="src/cellrenderer.cpp" line="219"></location>
+        <location filename="src/cellrenderer.cpp" line="219"/>
         <source>DISQ</source>
         <translation type="unfinished"></translation>
     </message>
