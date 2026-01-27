99faedb6f08ee231afe0c02d9ba2f35da264865e
Piotr Badarycz <pbadarycz@gmail.com>
2026-01-27 16:07:01 +0100

    Translated using Weblate (Polish)
    Currently translated at 100.0% (22 of 22 strings)
    
    Translation: QuickBox/libsiut
    Translate-URL: https://hosted.weblate.org/projects/quickbox/libsiut/pl/

--- libsiut/libsiut-pl_PL.ts
+++ libsiut/libsiut-pl_PL.ts
@@ -6,27 +6,27 @@
     <message>
         <location filename="src/sicard.cpp" line="38"/>
         <source>stationNumber: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>kod punktu kontrolnego:%1</translation>
     </message>
     <message>
         <location filename="src/sicard.cpp" line="39"/>
         <source>cardNumber: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>numer chipa:%1</translation>
     </message>
     <message>
         <location filename="src/sicard.cpp" line="43"/>
         <source>check: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>kontrola czipa:%1</translation>
     </message>
     <message>
         <location filename="src/sicard.cpp" line="44"/>
         <source>start: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>start: %1</translation>
     </message>
     <message>
         <location filename="src/sicard.cpp" line="45"/>
         <source>finish: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>meta:%1</translation>
     </message>
     <message>
         <location filename="src/sicard.cpp" line="46"/>
@@ -45,7 +45,13 @@ Handshake: {{HandShake}}
 Password access: {{PasswordAccess}}
 Read out after punch: {{ReadOutAfterPunch}}
 </source>
-        <translation type="unfinished"></translation>
+        <translation>Numer stacji: {{StationNumber}}
+Tryb rozszerzony: {{ExtendedMode}}
+Automatyczne wysyłanie: {{AutoSend}}
+Handshake: {{HandShake}}
+Hasło dostępu: {{PasswordAccess}}
+Wyczytanie po potwierdzeniu: {{ReadOutAfterPunch}}
+</translation>
     </message>
     <message>
         <location filename="src/device/sitask.cpp" line="104"/>
@@ -54,7 +60,7 @@ Read out after punch: {{ReadOutAfterPunch}}
         <location filename="src/device/sitask.cpp" line="107"/>
         <location filename="src/device/sitask.cpp" line="108"/>
         <source>True</source>
-        <translation type="unfinished"></translation>
+        <translation>Tak</translation>
     </message>
     <message>
         <location filename="src/device/sitask.cpp" line="104"/>
@@ -63,7 +69,7 @@ Read out after punch: {{ReadOutAfterPunch}}
         <location filename="src/device/sitask.cpp" line="107"/>
         <location filename="src/device/sitask.cpp" line="108"/>
         <source>False</source>
-        <translation type="unfinished"></translation>
+        <translation>Nie</translation>
     </message>
 </context>
 <context>
@@ -71,38 +77,39 @@ Read out after punch: {{ReadOutAfterPunch}}
     <message>
         <location filename="src/device/commport.cpp" line="41"/>
         <source>Available ports: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Dostępne porty: %1</translation>
     </message>
     <message>
         <location filename="src/device/commport.cpp" line="49"/>
         <source>Connecting to %1 - baudrate: %2, data bits: %3, parity: %4, stop bits: %5</source>
-        <translation type="unfinished"></translation>
+        <translation>Łączenie do %1 - baudrate: %2, data bits: %3, parity: %4, stop bits: %5</translation>
     </message>
     <message>
         <location filename="src/device/commport.cpp" line="59"/>
         <source>%1 connected OK</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 połączony OK</translation>
     </message>
     <message>
         <location filename="src/device/commport.cpp" line="63"/>
         <source>%1 connect ERROR: %2</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 błąd połączenia: %2</translation>
     </message>
     <message>
         <location filename="src/device/commport.cpp" line="73"/>
         <source>%1 closed</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 odłączony</translation>
     </message>
     <message>
         <location filename="src/device/commport.cpp" line="90"/>
         <source>possible solution:
 Wait at least 10 seconds and then try again.</source>
-        <translation type="unfinished"></translation>
+        <translation>możliwe rozwiązanie:
+Poczekaj przynajmniej 10 sekund i spróbuj ponownie.</translation>
     </message>
     <message>
         <location filename="src/device/commport.cpp" line="99"/>
         <source>There are no ports available.</source>
-        <translation type="unfinished"></translation>
+        <translation>Brak dostępnych portów.</translation>
     </message>
     <message>
         <location filename="src/device/commport.cpp" line="102"/>
@@ -110,7 +117,10 @@ Wait at least 10 seconds and then try again.</source>
 List of accessible ports:
 
 </source>
-        <translation type="unfinished"></translation>
+        <translation>Wybrany port %1 nie jest dostępny.
+Lista dostępnych portów:
+
+</translation>
     </message>
 </context>
 <context>
@@ -118,27 +128,27 @@ List of accessible ports:
     <message>
         <location filename="src/device/sidevicedriver.cpp" line="123"/>
         <source>Garbage received, stripping %1 characters from beginning of buffer</source>
-        <translation type="unfinished"></translation>
+        <translation>Odebrano nieprawidłowe dane, pominięto %1 znaków z początku bufora</translation>
     </message>
     <message>
         <location filename="src/device/sidevicedriver.cpp" line="140"/>
         <source>NAK received</source>
-        <translation type="unfinished"></translation>
+        <translation>odebrano NAK</translation>
     </message>
     <message>
         <location filename="src/device/sidevicedriver.cpp" line="146"/>
         <source>Legacy protocol is not supported, switch station to extended one.</source>
-        <translation type="unfinished"></translation>
+        <translation>Stary protokół nie jest obsługiwany, przełącz stację na rozszerzony protokół.</translation>
     </message>
     <message>
         <location filename="src/device/sidevicedriver.cpp" line="153"/>
         <source>Valid message shall end with ETX or NAK, throwing data away</source>
-        <translation type="unfinished"></translation>
+        <translation>Poprawny komunikat musi kończyć się ATX lub NAK, dane zostały odrzucone.</translation>
     </message>
     <message>
         <location filename="src/device/sidevicedriver.cpp" line="169"/>
         <source>SIDeviceDriver::sendCommand() - ERROR Sending of EXT commands only is supported for sending.</source>
-        <translation type="unfinished"></translation>
+        <translation>SIDeviceDriver::sendCommand() - BŁĄD - wspierane jest tylko przesyłanie komend EXT.</translation>
     </message>
 </context>
 <context>
@@ -146,7 +156,7 @@ List of accessible ports:
     <message>
         <location filename="src/device/sitask.cpp" line="24"/>
         <source>SiCommand timeout after %1 sec.</source>
-        <translation type="unfinished"></translation>
+        <translation>SiCommand timeout po %1 sec.</translation>
     </message>
 </context>
 </TS>
