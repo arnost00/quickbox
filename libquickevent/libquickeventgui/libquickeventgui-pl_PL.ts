f19e1129e061c8756843969d98d960b4fd3252be
Piotr Badarycz <pbadarycz@gmail.com>
2026-01-27 17:02:41 +0100

    Translated using Weblate (Polish)
    Currently translated at 100.0% (47 of 47 strings)
    
    Translation: QuickBox/libquickeventgui
    Translate-URL: https://hosted.weblate.org/projects/quickbox/libquickeventgui/pl/

--- libquickevent/libquickeventgui/libquickeventgui-pl_PL.ts
+++ libquickevent/libquickeventgui/libquickeventgui-pl_PL.ts
@@ -6,138 +6,138 @@
     <message>
         <location filename="src/reportoptionsdialog.ui" line="14"/>
         <source>Print Report Options</source>
-        <translation type="unfinished"></translation>
+        <translation>Ustawienia wydruku</translation>
     </message>
     <message>
         <location filename="src/reportoptionsdialog.ui" line="247"/>
         <source>Legs count</source>
-        <translation type="unfinished"></translation>
+        <translation>Numer przebiegu</translation>
     </message>
     <message>
         <location filename="src/reportoptionsdialog.ui" line="253"/>
         <source>Number of legs</source>
-        <translation type="unfinished"></translation>
+        <translation>Liczba przebiegów</translation>
     </message>
     <message>
         <location filename="src/reportoptionsdialog.ui" line="60"/>
         <source>Stages count</source>
-        <translation type="unfinished"></translation>
+        <translation>Numer etapu</translation>
     </message>
     <message>
         <location filename="src/reportoptionsdialog.ui" line="66"/>
         <source>Number of stages</source>
-        <translation type="unfinished"></translation>
+        <translation>Liczba etapów</translation>
     </message>
     <message>
         <location filename="src/reportoptionsdialog.ui" line="266"/>
         <source>Class filter</source>
-        <translation type="unfinished"></translation>
+        <translation>Filtr kategorii</translation>
     </message>
     <message>
         <location filename="src/reportoptionsdialog.ui" line="290"/>
         <source>Doesn&apos;t match</source>
-        <translation type="unfinished"></translation>
+        <translation>Nie spełnia</translation>
     </message>
     <message>
         <location filename="src/reportoptionsdialog.ui" line="297"/>
         <source>An question mark (?) in pattern stands for (matches) any single character; a asterisk (*) matches any sequence of zero or more characters.</source>
-        <translation type="unfinished"></translation>
+        <translation>Znak zapytania (?) we wzorcu, oznacza jeden dowolny znak; gwiazdka (*) oznacza dowolną sekwencję o długośći 0-n znaków.</translation>
     </message>
     <message>
         <location filename="src/reportoptionsdialog.ui" line="300"/>
         <source>Wild card</source>
-        <translation type="unfinished"></translation>
+        <translation>Dowolny znak</translation>
     </message>
     <message>
         <location filename="src/reportoptionsdialog.ui" line="310"/>
         <source>Posix regular expression</source>
-        <translation type="unfinished"></translation>
+        <translation>Wyrażenie regularne POSIX</translation>
     </message>
     <message>
         <location filename="src/reportoptionsdialog.ui" line="313"/>
         <source>RegExp</source>
-        <translation type="unfinished"></translation>
+        <translation>Wyrażenie regularne</translation>
     </message>
     <message>
         <location filename="src/reportoptionsdialog.ui" line="320"/>
         <source>Coma delimited list of class names</source>
-        <translation type="unfinished"></translation>
+        <translation>Lista kategorii oddzielonych przecinkami</translation>
     </message>
     <message>
         <location filename="src/reportoptionsdialog.ui" line="323"/>
         <location filename="src/reportoptionsdialog.ui" line="416"/>
         <source>Class names</source>
-        <translation type="unfinished"></translation>
+        <translation>Nazwy kategorii</translation>
     </message>
     <message>
         <location filename="src/reportoptionsdialog.ui" line="446"/>
         <source>Starters options</source>
-        <translation type="unfinished"></translation>
+        <translation>Ustawienia zawodników</translation>
     </message>
     <message>
         <location filename="src/reportoptionsdialog.ui" line="464"/>
         <source>Space after line</source>
-        <translation type="unfinished"></translation>
+        <translation>Spacja po linii</translation>
     </message>
     <message>
         <location filename="src/reportoptionsdialog.ui" line="474"/>
         <source>mm</source>
-        <translation type="unfinished"></translation>
+        <translation>mm</translation>
     </message>
     <message>
         <location filename="src/reportoptionsdialog.ui" line="539"/>
         <source>Relay options</source>
-        <translation type="unfinished"></translation>
+        <translation>Ustawienia sztafet</translation>
     </message>
     <message>
         <location filename="src/reportoptionsdialog.ui" line="545"/>
         <source>Show legs details</source>
-        <translation type="unfinished"></translation>
+        <translation>Pokaż szczegóły zmian</translation>
     </message>
     <message>
         <location filename="src/reportoptionsdialog.ui" line="336"/>
         <source>Start options</source>
-        <translation type="unfinished"></translation>
+        <translation>Ustawienia listy startowej</translation>
     </message>
     <message>
         <location filename="src/reportoptionsdialog.ui" line="354"/>
         <source>Print vacants</source>
-        <translation type="unfinished"></translation>
+        <translation>Drukuj wakaty</translation>
     </message>
     <message>
         <location filename="src/reportoptionsdialog.ui" line="361"/>
         <source>Print start numbers</source>
-        <translation type="unfinished"></translation>
+        <translation>Drukuj numery startowe</translation>
     </message>
     <message>
         <location filename="src/reportoptionsdialog.ui" line="368"/>
         <source>Use only class from selected start</source>
-        <translation type="unfinished"></translation>
+        <translation>Użyj kategorii z wybranego startu</translation>
     </message>
     <message>
         <location filename="src/reportoptionsdialog.ui" line="487"/>
         <source>Result options</source>
-        <translation type="unfinished"></translation>
+        <translation>Ustawienia wyników</translation>
     </message>
     <message>
         <location filename="src/reportoptionsdialog.ui" line="493"/>
         <source>Number of places in each class</source>
-        <translation type="unfinished"></translation>
+        <translation>Maksymalna liczba miejsc w każdej kategorii</translation>
     </message>
     <message>
         <location filename="src/reportoptionsdialog.ui" line="516"/>
         <source>Exclude DISQ</source>
-        <translation type="unfinished"></translation>
+        <translation>Oprócz NKL</translation>
     </message>
     <message>
         <location filename="src/reportoptionsdialog.ui" line="79"/>
         <source>Page layout</source>
-        <translation type="unfinished"></translation>
+        <translation>Ustawienia strony</translation>
     </message>
     <message>
         <location filename="src/reportoptionsdialog.ui" line="97"/>
         <source>Horizontal margin</source>
-        <translation type="unfinished"></translation>
+        <translation>Poziomy margines</translation>
     </message>
     <message>
         <location filename="src/reportoptionsdialog.ui" line="104"/>
@@ -145,105 +145,105 @@
         <location filename="src/reportoptionsdialog.ui" line="173"/>
         <location filename="src/reportoptionsdialog.ui" line="199"/>
         <source> mm</source>
-        <translation type="unfinished"></translation>
+        <translation> mm</translation>
     </message>
     <message>
         <location filename="src/reportoptionsdialog.ui" line="136"/>
         <source>Column count</source>
-        <translation type="unfinished"></translation>
+        <translation>Liczba kolumn</translation>
     </message>
     <message>
         <location filename="src/reportoptionsdialog.ui" line="143"/>
         <source>Vertical margin</source>
-        <translation type="unfinished"></translation>
+        <translation>Pionowy margines</translation>
     </message>
     <message>
         <location filename="src/reportoptionsdialog.ui" line="166"/>
         <source>Page width</source>
-        <translation type="unfinished"></translation>
+        <translation>Szerokość strony</translation>
     </message>
     <message>
         <location filename="src/reportoptionsdialog.ui" line="192"/>
         <source>Page height</source>
-        <translation type="unfinished"></translation>
+        <translation>Wysokość strony</translation>
     </message>
     <message>
         <location filename="src/reportoptionsdialog.ui" line="218"/>
         <source>Break type</source>
-        <translation type="unfinished"></translation>
+        <translation>Rodzaj łamania</translation>
     </message>
     <message>
         <location filename="src/reportoptionsdialog.ui" line="226"/>
         <source>None</source>
-        <translation type="unfinished"></translation>
+        <translation>Żadne</translation>
     </message>
     <message>
         <location filename="src/reportoptionsdialog.ui" line="231"/>
         <source>Column</source>
-        <translation type="unfinished"></translation>
+        <translation>Kolumna</translation>
     </message>
     <message>
         <location filename="src/reportoptionsdialog.ui" line="236"/>
         <source>Page</source>
-        <translation type="unfinished"></translation>
+        <translation>Strona</translation>
     </message>
     <message>
         <location filename="src/reportoptionsdialog.ui" line="34"/>
         <source>Save as default</source>
-        <translation type="unfinished"></translation>
+        <translation>Zapisz jako domyślne</translation>
     </message>
     <message>
         <location filename="src/reportoptionsdialog.ui" line="41"/>
         <source>Reset</source>
-        <translation type="unfinished"></translation>
+        <translation>Resetuj</translation>
     </message>
     <message>
         <location filename="src/reportoptionsdialog.ui" line="407"/>
         <source>Start list is sorted first by</source>
-        <translation type="unfinished"></translation>
+        <translation>Lista startowa jest sortowana po</translation>
     </message>
     <message>
         <location filename="src/reportoptionsdialog.ui" line="413"/>
         <source>(class name, start time, runner name)</source>
         <oldsource>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;(class name, start time,  runner name)&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</oldsource>
-        <translation type="unfinished"></translation>
+        <translation>(nazwa kategorii, czas startu, nazwisko zawodnika)</translation>
     </message>
     <message>
         <location filename="src/reportoptionsdialog.ui" line="423"/>
         <source>(start time, class name, runner name)</source>
         <oldsource>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;(start time, class name, runner name)&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</oldsource>
-        <translation type="unfinished"></translation>
+        <translation>(czas startu, nazwa kategorii, nazwisko zawodnika)</translation>
     </message>
     <message>
         <location filename="src/reportoptionsdialog.ui" line="426"/>
         <source>Start times</source>
-        <translation type="unfinished"></translation>
+        <translation>Czasy startu</translation>
     </message>
     <message>
         <location filename="src/reportoptionsdialog.ui" line="433"/>
         <source>(runner name, class name, start time)</source>
         <oldsource>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;(runner name, class name, start time)&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</oldsource>
-        <translation type="unfinished"></translation>
+        <translation>(nazwisko zawodnika, nazwa kategorii, czas startu)</translation>
     </message>
     <message>
         <location filename="src/reportoptionsdialog.ui" line="436"/>
         <source>Runner names</source>
-        <translation type="unfinished"></translation>
+        <translation>Nazwiska zawodników</translation>
     </message>
     <message>
         <location filename="src/reportoptionsdialog.ui" line="381"/>
         <source>Start time format</source>
-        <translation type="unfinished"></translation>
+        <translation>Format czasu startu</translation>
     </message>
     <message>
         <location filename="src/reportoptionsdialog.ui" line="387"/>
         <source>start at 0 (mmm.ss)  </source>
-        <translation type="unfinished"></translation>
+        <translation>start od 0 (mmm.ss)  </translation>
     </message>
     <message>
         <location filename="src/reportoptionsdialog.ui" line="397"/>
         <source>daytime (hh:mm:ss)</source>
-        <translation type="unfinished"></translation>
+        <translation>dzienny czas (hh:mm:ss)</translation>
     </message>
 </context>
 </TS>
