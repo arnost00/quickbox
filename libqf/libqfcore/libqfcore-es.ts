9e750391e08b1e2d1cb3389d4725abb459f56744
pimatherror <pi.matherror@gmail.com>
2026-04-09 23:19:14 +0200

    Translated using Weblate (Spanish)
    Currently translated at 75.0% (3 of 4 strings)
    
    Translation: QuickBox/libqf: core
    Translate-URL: https://hosted.weblate.org/projects/quickbox/libqf-core/es/

--- libqf/libqfcore/libqfcore-es.ts
+++ libqf/libqfcore/libqfcore-es.ts
@@ -7,7 +7,7 @@
         <location filename="src/sql/catalog.cpp" line="262"/>
         <location filename="src/sql/catalog.cpp" line="280"/>
         <source>Found info for nonexisting field &apos;%1&apos;</source>
-        <translation type="unfinished"></translation>
+        <translation>Información encontrada para el campo inexistente &apos;%1&apos;</translation>
     </message>
 </context>
 <context>
@@ -20,12 +20,12 @@
     <message>
         <location filename="src/utils/clioptions.cpp" line="99"/>
         <source>Print help</source>
-        <translation type="unfinished"></translation>
+        <translation>Imprimir ayuda</translation>
     </message>
     <message>
         <location filename="src/utils/clioptions.cpp" line="100"/>
         <source>Config name, it is loaded from {app-name}[.conf] if file exists in {config-path}</source>
-        <translation type="unfinished"></translation>
+        <translation>Nombre de configuración, se ha cargado desde {app-name}[.conf] si existe el fichero en {config-path}</translation>
     </message>
 </context>
 </TS>
