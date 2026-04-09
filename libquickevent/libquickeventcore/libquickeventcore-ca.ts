801cfcffa47b3f621efa913298934722ac8b5ef8
pimatherror <pi.matherror@gmail.com>
2026-04-10 00:48:14 +0200

    Added translation using Weblate (Catalan)

---
+++ libquickevent/libquickeventcore/libquickeventcore-ca.ts
@@ -0,0 +1,184 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ca">
+<context>
+    <name>RunStatus</name>
+    <message>
+        <location filename="src/runstatus.cpp" line="121"/>
+        <source>DISQ</source>
+        <comment>Disqualified</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/runstatus.cpp" line="123"/>
+        <source>MP</source>
+        <comment>Missing Punch</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/runstatus.cpp" line="125"/>
+        <source>DNS</source>
+        <comment>Did Not Start</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/runstatus.cpp" line="127"/>
+        <source>DNF</source>
+        <comment>Did Not Finish</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/runstatus.cpp" line="129"/>
+        <source>OVRT</source>
+        <comment>Over Time</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/runstatus.cpp" line="119"/>
+        <source>NC</source>
+        <comment>Not Competing</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/runstatus.cpp" line="130"/>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ogtime</name>
+    <message>
+        <source>DISQ</source>
+        <translation type="vanished">DISK</translation>
+    </message>
+    <message>
+        <source>MP</source>
+        <translation type="vanished">MP</translation>
+    </message>
+    <message>
+        <source>NC</source>
+        <translation type="vanished">MS</translation>
+    </message>
+    <message>
+        <source>DNF</source>
+        <translation type="vanished">DNF</translation>
+    </message>
+    <message>
+        <source>DNS</source>
+        <translation type="vanished">DNS</translation>
+    </message>
+    <message>
+        <source>OVRT</source>
+        <translation type="vanished">OVRT</translation>
+    </message>
+</context>
+<context>
+    <name>quickevent::core::exporters::HtmlFileExporter</name>
+    <message>
+        <location filename="src/exporters/htmlfileexporter.cpp" line="60"/>
+        <source>E%1 %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>quickevent::core::exporters::StageResultsHtmlExporter</name>
+    <message>
+        <location filename="src/exporters/stageresultshtmlexporter.cpp" line="20"/>
+        <source>Results</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/exporters/stageresultshtmlexporter.cpp" line="42"/>
+        <source>E%1 %2 %3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/exporters/stageresultshtmlexporter.cpp" line="46"/>
+        <source> length: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/exporters/stageresultshtmlexporter.cpp" line="46"/>
+        <source>climb: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/exporters/stageresultshtmlexporter.cpp" line="64"/>
+        <source>Pos</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/exporters/stageresultshtmlexporter.cpp" line="65"/>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/exporters/stageresultshtmlexporter.cpp" line="66"/>
+        <source>Registration</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/exporters/stageresultshtmlexporter.cpp" line="67"/>
+        <source>Time</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/exporters/stageresultshtmlexporter.cpp" line="68"/>
+        <source>Status</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/exporters/stageresultshtmlexporter.cpp" line="101"/>
+        <source>Results %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>quickevent::core::exporters::StageStartListHtmlExporter</name>
+    <message>
+        <location filename="src/exporters/stagestartlisthtmlexporter.cpp" line="19"/>
+        <source>Start List</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/exporters/stagestartlisthtmlexporter.cpp" line="41"/>
+        <source>E%1 %2 Start list</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/exporters/stagestartlisthtmlexporter.cpp" line="45"/>
+        <source> length: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/exporters/stagestartlisthtmlexporter.cpp" line="45"/>
+        <source>climb: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/exporters/stagestartlisthtmlexporter.cpp" line="63"/>
+        <source>Start</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/exporters/stagestartlisthtmlexporter.cpp" line="64"/>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/exporters/stagestartlisthtmlexporter.cpp" line="65"/>
+        <source>Registration</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/exporters/stagestartlisthtmlexporter.cpp" line="66"/>
+        <source>SI</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/exporters/stagestartlisthtmlexporter.cpp" line="89"/>
+        <source>%1 %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
