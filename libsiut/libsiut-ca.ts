95fe1c54fa2eab2e050b19ce660dca4c722010d6
pimatherror <pi.matherror@gmail.com>
2026-04-10 00:48:16 +0200

    Added translation using Weblate (Catalan)

---
+++ libsiut/libsiut-ca.ts
@@ -0,0 +1,152 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ca">
+<context>
+    <name>SICard</name>
+    <message>
+        <location filename="src/sicard.cpp" line="38"/>
+        <source>stationNumber: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/sicard.cpp" line="39"/>
+        <source>cardNumber: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/sicard.cpp" line="43"/>
+        <source>check: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/sicard.cpp" line="44"/>
+        <source>start: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/sicard.cpp" line="45"/>
+        <source>finish: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/sicard.cpp" line="46"/>
+        <source>batteryStatus: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SiStationConfig</name>
+    <message>
+        <location filename="src/device/sitask.cpp" line="95"/>
+        <source>Station number: {{StationNumber}}
+Extended mode: {{ExtendedMode}}
+Auto send: {{AutoSend}}
+Handshake: {{HandShake}}
+Password access: {{PasswordAccess}}
+Read out after punch: {{ReadOutAfterPunch}}
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/device/sitask.cpp" line="104"/>
+        <location filename="src/device/sitask.cpp" line="105"/>
+        <location filename="src/device/sitask.cpp" line="106"/>
+        <location filename="src/device/sitask.cpp" line="107"/>
+        <location filename="src/device/sitask.cpp" line="108"/>
+        <source>True</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/device/sitask.cpp" line="104"/>
+        <location filename="src/device/sitask.cpp" line="105"/>
+        <location filename="src/device/sitask.cpp" line="106"/>
+        <location filename="src/device/sitask.cpp" line="107"/>
+        <location filename="src/device/sitask.cpp" line="108"/>
+        <source>False</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>siut::CommPort</name>
+    <message>
+        <location filename="src/device/commport.cpp" line="41"/>
+        <source>Available ports: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/device/commport.cpp" line="49"/>
+        <source>Connecting to %1 - baudrate: %2, data bits: %3, parity: %4, stop bits: %5</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/device/commport.cpp" line="59"/>
+        <source>%1 connected OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/device/commport.cpp" line="63"/>
+        <source>%1 connect ERROR: %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/device/commport.cpp" line="73"/>
+        <source>%1 closed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/device/commport.cpp" line="90"/>
+        <source>possible solution:
+Wait at least 10 seconds and then try again.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/device/commport.cpp" line="99"/>
+        <source>There are no ports available.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/device/commport.cpp" line="102"/>
+        <source>Selected port %1 is not available.
+List of accessible ports:
+
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>siut::DeviceDriver</name>
+    <message>
+        <location filename="src/device/sidevicedriver.cpp" line="123"/>
+        <source>Garbage received, stripping %1 characters from beginning of buffer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/device/sidevicedriver.cpp" line="140"/>
+        <source>NAK received</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/device/sidevicedriver.cpp" line="146"/>
+        <source>Legacy protocol is not supported, switch station to extended one.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/device/sidevicedriver.cpp" line="153"/>
+        <source>Valid message shall end with ETX or NAK, throwing data away</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/device/sidevicedriver.cpp" line="169"/>
+        <source>SIDeviceDriver::sendCommand() - ERROR Sending of EXT commands only is supported for sending.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>siut::SiTask</name>
+    <message>
+        <location filename="src/device/sitask.cpp" line="24"/>
+        <source>SiCommand timeout after %1 sec.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
